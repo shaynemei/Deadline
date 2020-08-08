db2cc72ef7f9b6741adf82bbc6d9c80c988a94e8
Shayne <chmei@uw.edu>
2020-08-09 03:04:29 +0800

    refactor outdated sqlite test code

--- tests/data.sql
+++
@@ -1,8 +0,0 @@
-INSERT INTO user (username, password)
-VALUES
-  ('test', 'pbkdf2:sha256:50000$TCI4GzcX$0de171a4f4dac32e3364c7ddc7c14f3e2fa61f2d17574483f7ffbb431b4acb2f'),
-  ('other', 'pbkdf2:sha256:50000$kJPKsz6N$d2d4784f1b030a9761f5ccaeeaca413f27f2ecb76d6168407af962ddce849f79');
-
-INSERT INTO task (author_id, created, body)
-VALUES
-  (1, '2020-06-24 00:12:05', x'0a' || 'do laundry');
