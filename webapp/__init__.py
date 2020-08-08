18966a7b8277245dc6aaa21c7884a0ccef3f2b3e
Shayne <chmei@uw.edu>
2020-08-09 03:04:04 +0800

    refactor and connect to firebase

--- webapp/__init__.py
+++ webapp/__init__.py
@@ -1,43 +1,78 @@
-import os
-from flask import Flask, render_template
+from flask import Flask
+from flask import render_template
+from flask import request
+from firebase_admin import auth
+from . import db
+from functools import wraps
 
 
-def create_app(test_config=None):
-    # create and configure the app
-    app = Flask(__name__, instance_relative_config=True)
+# init the app instance
+app = Flask(__name__)
 
-    # Todo: SECRET_KEY should be overridden with a random value when deploying, e.g. a long random string of bytes
-    app.config.from_mapping(
-        # for signing session cookies or any other security related needs by extensions or your application
-        # set to 'dev' for development convenience
-        SECRET_KEY='dev',
-        # Path to database file
-        DATABASE=os.path.join(app.instance_path, 'webapp.sqlite'),
-    )
+# init connection to Firebase
+# pb is used for db operations
+# firebase is an admin sdk for validating tokens
+firebase, pb = db.init_db()
 
-    if test_config is None:
-        # load the instance config, if it exists, when not testing
-        app.config.from_pyfile('config.py', silent=True)
-    else:
-        # load the test config if passed in
-        app.config.from_mapping(test_config)
 
-    # ensure the instance folder exists
+# middleware to make sure only valid users can perform CRUD
+def check_token(f):
+    @wraps(f)
+    def wrap(*args,**kwargs):
+        if not request.headers.get('authorization'):
+            return {'message': 'No token provided'}, 400
+        try:
+            user = auth.verify_id_token(request.headers['authorization'])
+            request.user = user
+        except:
+            return {'message':'Invalid token provided.'}, 400
+        return f(*args, **kwargs)
+    return wrap
+
+
+# a hello page for testing
+@app.route('/hello')
+def hello():
+    return "Hello World!"
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+# Test route to check token
+@app.route('/api/userinfo')
+@check_token
+def userinfo():
+    return {'data': "TEST USER DATA"}, 200
+
+
+# Api route to sign up a new user
+@app.route('/api/signup')
+def signup():
+    email = request.form.get('email')
+    password = request.form.get('password')
+    if email is None or password is None:
+        return {'message': 'Error missing email or password'}, 400
     try:
-        os.makedirs(app.instance_path)
-    except OSError:
-        pass
+        user = auth.create_user(
+            email=email,
+            password=password
+        )
+        return {'message': f'Successfully created user {user.uid}'}, 200
+    except Exception as e:
+        return {'message': e}, 400
 
-    # a hello page for testing
-    @app.route('/hello')
-    def hello():
-        return "Hello World!"
 
-    @app.route('/')
-    def index():
-        return render_template('index.html')
-
-    from . import db
-    db.init_app(app)
-
-    return app
+# Api route to get a new token for a valid user
+@app.route('/api/token')
+def token():
+    email = request.form.get('email')
+    password = request.form.get('password')
+    try:
+        user = pb.auth().sign_in_with_email_and_password(email, password)
+        jwt = user['idToken']
+        return {'token': jwt}, 200
+    except Exception as e:
+        return {'message': e}, 400
